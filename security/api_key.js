a7768e0d16abdf2ce5547ca380be23e269b7def1
David Landry <david@dmwl.net>
2017-02-18 20:37:39 -0500

    Check that authorization header is set before using it

--- security/api_key.js
+++ security/api_key.js
@@ -10,7 +10,13 @@ var base64 = require('base-64');
 var api_key = require('../config/secrets.js').api_key;
 
 module.exports = function authorize(req, res, next) {
-  var token = base64.decode(req.headers.authorization.split(" ")[1]);
+  var auth = req.headers.authorization;
+  if (auth == undefined) {
+    next(createError(401, "Missing or incorrect api key"));
+    return;
+  }
+
+  var token = base64.decode(auth.split(" ")[1]);
   if( token != api_key ) {
     next(createError(401, "Missing or incorrect api key"));
   }

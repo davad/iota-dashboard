c0e4ce278ac386d9011f39c2c28f59097fe4a042
David Landry <david@dmwl.net>
2017-02-26 01:23:16 -0500

    Change iota logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../assets/iota_logo.png';
 
 const Header = () => {
   return (
     <div className="top_nav">
         <div className="nav_menu">
             <nav className="dashboardHeader">
-                <img src="http://iotatoken.com/img/logo.png" height="102px"/>
+                <img src={logo} height="150"/>
             </nav>
         </div>
     </div>

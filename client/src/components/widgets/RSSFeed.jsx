0bbfe24360dac4febea6a4eb543b436de1285d8b
David Landry <david@dmwl.net>
2017-02-26 00:56:10 -0500

    Add RSSFeed component similar to the one in controlfrog

---
+++ client/src/components/widgets/RSSFeed.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+
+class RSSFeed extends Component {
+  static propTypes = {
+    transactions: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  }
+
+  formatSlides(transactions) {
+    return transactions.map( (tx, index) => {
+      return (
+        <blockquote key={tx.index}>
+        {tx.sender} sent {tx.value} IOTA to {tx.recipient}
+        </blockquote>
+      );
+    });
+  }
+
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      beforeChange: function (currentSlide, nextSlide) {
+        console.log(new Date().toLocaleString(), 'before change', currentSlide, nextSlide);
+      },
+      afterChange: function (currentSlide) {
+        console.log('after change', currentSlide);
+      },
+    };
+
+    let transactions = this.props.transactions;
+
+    const slides = (transactions) ? this.formatSlides(this.props.transactions) : <div key='empty' />;
+
+    return (
+      <Slider {...settings}>
+        {slides}
+      </Slider>
+    );
+  }
+}
+
+export default RSSFeed;
